af043d049ab186fe06b5ce1ed53293fca0827b90
SubehnduKumar <bhsubehndukumar66@gmail.com>
2023-01-30 15:22:13 +0530

    express validator with sanitizer || matchedData ||check

--- expressValidator2.js
+++ expressValidator2.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require("body-parser")
 // ...rest of the initial code omitted for simplicity.
-const { body, validationResult, check } = require('express-validator');
+const { body, validationResult, check, matchedData } = require('express-validator');
 
 const app = express()
 const port = 3001
@@ -43,13 +43,36 @@ app.get("/", (req, res) => {
 
 
 app.post("/", urlencodedParser, [
-    check('username', 'Username email id').isEmail(),
-    check("password", "must be in 5 char").isLength({ min: 5 })
+    //before using sanitizer
+    // check('username', 'Username email id fake').isEmail(),
+    // check("password", "must be in 5 char").isLength({ min: 5 }),
+    // check("cpassword", "must be in 5 char").isLength({ min: 5 })
+    //after using sanitizer
+    check('username', 'Username email id fake').trim().isEmail(),
+    check("password", "must be in 5 char").trim().isLength({ min: 5 }),
+    // check("cpassword", "must be in 5 char").trim().isLength({ min: 5 }),
+    check("cpassword").custom((value, { req }) => {
+        if (value != req.body.password) {
+            throw new Error("confirm password not matching")
+        }
+        return true; //need to give this one
+    })
 ], (req, res) => {
-    const errors=validationResult(req)
+    const errors = validationResult(req)
     console.log(errors.mapped());
-    console.log(req.body);
-    res.render("indexForValidator", { title: "User Details",error:errors.mapped()})
+    if (!errors.isEmpty()) {
+        const userData = matchedData(req)
+        // res.render("indexForValidator", { title: "User Details", error: errors.mapped() })
+        res.render("indexForValidator", { title: "User Details", error: errors.mapped(), userData: userData })
+    }
+    else {
+        const userData = matchedData(req)
+        console.log("userData", userData);
+        // res.render("login", { title: "User Details", username:req.body.username,password:req.body.password })
+        res.render("login", { title: "User Details", userData: userData })
+    }
+    // console.log(req.body);
+    // res.render("indexForValidator", { title: "User Details", error: errors.mapped() })
 })
 
 
